a8a450ec8144f6ea347b29b1a0f1517a8d44897e
Emre <emrsmsrli1@gmail.com>
2016-11-06 22:46:36 +0100

    refactor show recipe 2

--- app/src/main/java/se/ju/student/android_mjecipes/MainActivity.java
+++ app/src/main/java/se/ju/student/android_mjecipes/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getBaseContext(), LoginActivity.class));
+                startActivity(new Intent(getBaseContext(), ShowRecipeActivity.class));
             }
         });
 
