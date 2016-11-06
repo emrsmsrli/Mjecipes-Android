6a3585a53250533d06040a1509abe65f89a04cd9
Emre <emrsmsrli1@gmail.com>
2016-11-06 22:45:28 +0100

    refactor showrecipe

--- app/src/main/java/se/ju/student/android_mjecipes/ShowRecipeActivity.java
+++ app/src/main/java/se/ju/student/android_mjecipes/ShowRecipeActivity.java
@@ -1,20 +1,23 @@
 package se.ju.student.android_mjecipes;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class ShowRecipe extends AppCompatActivity {
+public class ShowRecipeActivity extends AppCompatActivity {
 
     ImageView recipeImage;
-    TextView recipeName;
     TextView recipeDesc;
 
+    LinearLayout directions;
+
     Button comments;
-    Button directions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +25,19 @@ public class ShowRecipe extends AppCompatActivity {
         setContentView(R.layout.activity_show_recipe);
 
         recipeImage = (ImageView) findViewById(R.id.show_recipe_img);
-        recipeName = (TextView) findViewById(R.id.show_recipe_name);
         recipeDesc = (TextView) findViewById(R.id.show_recipe_desc);
-
-        directions = (Button) findViewById(R.id.show_recipes_b_directions);
         comments = (Button) findViewById(R.id.show_recipes_b_comments);
+        directions = (LinearLayout) findViewById(R.id.show_recipes_ll_directions);
 
-        directions.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO intent directions activity
-            }
-        });
+        for(int i = 0; i < 30; ++i) {
+            TextView tv = new TextView(getBaseContext());
+            tv.setText("sjkfhaşskjfsa");
+            tv.setPadding(0,0,0,10);
+            tv.setTextColor(Color.rgb(128,128,128));
+            directions.addView(tv);
+            setTitle("asdasd"); //TODO set recipe name
+        }
+
         comments.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
