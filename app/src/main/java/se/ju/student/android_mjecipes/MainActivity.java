1337732b7ec1de8f103bfd50249387a5527d0466
algi164a <algi164a@student.ju.se>
2016-11-06 16:04:10 +0100

    options menu and account class added.

--- app/src/main/java/se/ju/student/android_mjecipes/MainActivity.java
+++ app/src/main/java/se/ju/student/android_mjecipes/MainActivity.java
@@ -1,13 +1,61 @@
 package se.ju.student.android_mjecipes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id=item.getItemId();
+
+        if(id==R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+    private Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b= (Button) findViewById(R.id.butforsignin);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toy= new Intent(MainActivity.this,Sign_In.class);
+               startActivity(toy);
+            }
+        });
+
+
+
+
+
     }
+
+
+
+
+
+
 }
+
+
---
+++ app/src/main/java/se/ju/student/android_mjecipes/Sign_In.java
@@ -0,0 +1,25 @@
+package se.ju.student.android_mjecipes;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+
+import java.util.List;
+
+public class Sign_In extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign__in);
+
+
+        Account a=new Account();
+
+
+    }
+
+
+}
