29eb9f490eed7e0a6a39761482009a2cc368ded4
Emre <emrsmsrli1@gmail.com>
2016-11-09 15:24:17 +0100

    change const strings

--- app/src/main/java/se/ju/student/android_mjecipes/APIHandler/Handler.java
+++ app/src/main/java/se/ju/student/android_mjecipes/APIHandler/Handler.java
@@ -5,10 +5,10 @@ import com.google.gson.Gson;
 public abstract class Handler {
 
     static String API_URL = "http://52.211.99.140/api/v1/";
-    static final String accounts = "accounts/";
-    static final String tokens = "tokens/password";
-    static final String recipes = "recipes/";
-    static final String comments = "comments/";
+    static final String ACCOUNTS_URL = "accounts/";
+    static final String TOKENS_URL = "tokens/password";
+    static final String RECIPES_URL = "recipes/";
+    static final String COMMENTS_URL = "comments/";
     static Gson gson = new Gson();
 
     Errors errors;
