f68173d9aa337fbc79af43479080928c7e6b727e
Emre <emrsmsrli1@gmail.com>
2016-11-10 18:06:50 +0100

    extend jwtoken entity so we can get userid's

--- app/src/main/java/se/ju/student/android_mjecipes/Entities/JWToken.java
+++ app/src/main/java/se/ju/student/android_mjecipes/Entities/JWToken.java
@@ -1,10 +1,26 @@
 package se.ju.student.android_mjecipes.Entities;
 
+import android.util.Base64;
+
+import com.google.gson.Gson;
+
 public class JWToken {
+    private class JWTBody {
+        public String userId;
+    }
+
+    public static final int HEADER_INDEX = 0;
+    public static final int BODY_INDEX = 1;
+    public static final int SIGNATURE_INDEX = 2;
+
     public String access_token;
     public long expires_in;
 
     public String[] getPieces() {
-        return access_token.split(":");
+        return access_token.split("\\.");
+    }
+
+    public String getUserID() {
+        return new Gson().fromJson(new String(Base64.decode(getPieces()[BODY_INDEX], Base64.DEFAULT)), JWTBody.class).userId;
     }
 }
