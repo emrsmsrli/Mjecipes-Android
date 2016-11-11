34195f901d04a271fb1d2f5eafc889cde40cd66c
algi164a <algi164a@student.ju.se>
2016-11-12 00:29:18 +0100

    create recipe activity

--- app/src/main/java/se/ju/student/android_mjecipes/CreateRecipe.java
+++ app/src/main/java/se/ju/student/android_mjecipes/CreateRecipe.java
@@ -2,12 +2,35 @@ package se.ju.student.android_mjecipes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import se.ju.student.android_mjecipes.MjepicesAPIHandler.Entities.Recipe;
 
 public class CreateRecipe extends AppCompatActivity {
+
+        EditText name,direction,description;
+        Button createrecipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_recipe);
+
+
+
+        name=(EditText) findViewById(R.id.recipenamefield);
+        direction=(EditText) findViewById(R.id.directionfield);
+        description= (EditText) findViewById(R.id.descriptionfield);
+
+        createrecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Recipe r=new Recipe();
+
+
+            }
+        });
     }
 }
