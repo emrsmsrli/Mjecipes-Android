167d55cc9bae59cd1397704eda4e26d46b77ace2
algi164a <algi164a@student.ju.se>
2016-11-12 00:07:22 +0100

    create recipe activity

---
+++ app/src/main/java/se/ju/student/android_mjecipes/CreateRecipe.java
@@ -0,0 +1,13 @@
+package se.ju.student.android_mjecipes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateRecipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_recipe);
+    }
+}
