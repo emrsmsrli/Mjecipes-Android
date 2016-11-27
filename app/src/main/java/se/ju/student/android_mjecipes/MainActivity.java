acd377311970aaa09c50d327c95a56b95c3660a6
algi164a <algi164a@student.ju.se>
2016-11-28 00:56:02 +0100

    account delete

--- app/src/main/java/se/ju/student/android_mjecipes/MainActivity.java
+++ app/src/main/java/se/ju/student/android_mjecipes/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.AsyncTask;
 import android.os.UserHandle;
 import android.support.design.internal.NavigationMenu;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -37,7 +38,9 @@ import java.util.Date;
 import java.util.Locale;
 
 import se.ju.student.android_mjecipes.CacheHandlers.CacheHandler;
+import se.ju.student.android_mjecipes.MjepicesAPIHandler.Entities.JWToken;
 import se.ju.student.android_mjecipes.MjepicesAPIHandler.Entities.Recipe;
+import se.ju.student.android_mjecipes.MjepicesAPIHandler.Errors;
 import se.ju.student.android_mjecipes.MjepicesAPIHandler.Handler;
 import se.ju.student.android_mjecipes.UserAgent.UserAgent;
 
@@ -71,6 +74,170 @@ public static String a;
         onSearchRequested();
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Intent i=this.getIntent();
+        String action= i.getAction();
+
+        if(!(action==Intent.ACTION_DELETE)) {
+            if (!(action == Intent.ACTION_VIEW)) {
+                if (!UserAgent.getInstance(this).isLoggedIn())
+                    whilenotloginjobs();
+
+
+                else
+                    whileloginjobs();
+            } else {
+                if (!UserAgent.getInstance(this).isLoggedIn())
+                    whilenotloginjobs();
+
+
+                else
+                    showaccountrecipes();
+            }
+        }
+
+        else
+            deleteaccount();
+    }
+
+
+    void deleteaccount(){
+
+        setContentView(R.layout.activity_main);
+
+
+
+        final LinearLayout l= (LinearLayout) findViewById(R.id.activity_main);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        Toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+
+        drawerLayout.addDrawerListener(Toggle);
+        Toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+
+                    case R.id.myaccount:
+                        Intent i1 = new Intent(MainActivity.this, LoginActivity.class);
+
+                        startActivity(i1);
+                        drawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.myrecipes:
+                        Intent i2 = new Intent(MainActivity.this, LoginActivity.class);
+                        startActivity(i2);
+                        drawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.createarecipe:
+                        Intent i3 = new Intent(getApplicationContext(), LoginActivity.class);
+                        startActivity(i3);
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.recipeofday:
+                        Intent i4 = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i4);
+                        drawerLayout.closeDrawers();
+                        break;
+
+
+                    case R.id.signup:
+                        Intent i6 = new Intent(MainActivity.this, SignupActivity.class);
+                        int sayi = ((int) (Math.random() * 3));
+                        i6.putExtra("recipeId", Integer.toString(sayi));
+                        startActivity(i6);
+                        drawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.login:
+
+                        Intent i7 = new Intent(MainActivity.this, LoginActivity.class);
+
+                        startActivity(i7);
+                        drawerLayout.closeDrawers();
+
+                        break;
+
+
+                }
+
+                return false;
+            }
+        });
+
+
+
+        new AsyncTask<Void,Void,Recipe[]>(){
+            @Override
+            protected Recipe[] doInBackground(Void... params) {
+                JWToken token = Handler.getTokenHandler().getToken(UserAgent.getInstance(getBaseContext()).getUsername(),
+                        UserAgent.getInstance(getBaseContext()).getPassword());
+                Boolean b=Handler.getAccountHandler().deleteAccount(UserAgent.getInstance(getBaseContext()).getUserID(),token);
+                UserAgent.getInstance(getBaseContext()).logout();
+
+                if(b==true) {
+                    Snackbar.make(l, "Account deleted", Snackbar.LENGTH_SHORT).show();
+
+                }
+                else{
+                    Errors e = Handler.getRecipeHandler().getErrors();
+
+                }
+                return Handler.getRecipeHandler().getRecipeByPage(1);
+            }
+
+            @Override
+            protected void onPostExecute(Recipe[] recipes) {
+
+                LayoutInflater inf = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+
+                for (int i = 0; i < recipes.length; ++i) {
+                    inf.inflate(R.layout.main_recipe_layout, (LinearLayout) findViewById(R.id.activity_main));
+                    final View vv = ((LinearLayout) findViewById(R.id.activity_main)).getChildAt(i);
+                    ((TextView) vv.findViewById(R.id.main_recipe_id)).setText(Integer.toString(recipes[i].id));
+                    ((TextView) vv.findViewById(R.id.main_recipe_name)).setText("Name= " + recipes[i].name);
+                    ((TextView) vv.findViewById(R.id.main_recipe_date)).setText(sdf.format(new Date(recipes[i].created * 1000)));
+                    ((TextView) vv.findViewById(R.id.main_recipe_description)).setText("Description= " + recipes[i].description);
+                    ((TextView) vv.findViewById(R.id.main_recipe_creatorname)).setText("Creator= " + recipes[i].creator.userName);
+                    if (recipes[i].image != null) {
+                        final ImageView iv = (ImageView) vv.findViewById(R.id.main_recipe_image);
+                        CacheHandler.getImageCacheHandler(getBaseContext()).downloadImage(new ImageRequest(recipes[i].image, new Response.Listener<Bitmap>() {
+                            @Override
+                            public void onResponse(Bitmap response) {
+                                iv.setImageBitmap(response);
+                                iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                            }
+                        }, iv.getWidth(), iv.getHeight(), null, null, null));
+                    }
+
+                    vv.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                            Intent i = new Intent(getApplicationContext(), ShowRecipeActivity.class);
+                            i.putExtra("recipeId", ((TextView) vv.findViewById(R.id.main_recipe_id)).getText());
+                            startActivity(i);
+                        }
+                    });
+                }
+
+            }
+        }.execute();
+
+
     }
 
 
@@ -369,30 +536,6 @@ public static String a;
     }
 
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        Intent i=this.getIntent();
-        String action= i.getAction();
-        if(!(action==Intent.ACTION_VIEW)) {
-            if (!UserAgent.getInstance(this).isLoggedIn())
-                whilenotloginjobs();
-
-
-            else
-                whileloginjobs();
-        }
-
-        else {
-            if (!UserAgent.getInstance(this).isLoggedIn())
-                whilenotloginjobs();
-
-
-            else
-                showaccountrecipes();
-        }
-    }
 
 
 
